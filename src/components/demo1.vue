0b2b438a398b260ade408dfb5a1af7e612a2424a
unknown <1012883981@qq.com>
2020-05-18 20:50:54 +0800

    demo1

---
+++ src/components/demo1.vue
@@ -0,0 +1,13 @@
+<template>
+     <div>this is demo1</div>
+</template>
+
+<script>
+export default {
+     
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Demo1 from '../components/Demo1.vue'
 
 Vue.use(VueRouter)
-
-  const routes = [
+// 路由规则
+const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/demo1'
+  },
+  {
+    path: '/demo1',
+    name: 'Demo1',
+    component: Demo1
   }
 ]
 
